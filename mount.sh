7517983e29746daba2ff9747f568c28928ebacca
deeptir <deeptir@cs.stanford.edu>
2023-06-29 10:55:36 -0700

    update mount

--- mount.sh
+++ mount.sh
@@ -9,18 +9,14 @@ for user in $USERS; do
     else
         ssh_dir=/users/$user/.ssh
     fi
-    mkdir $ssh_dir && chmod 700 $ssh_dir
-    /usr/bin/geni-get key > $ssh_dir/id_rsa
-    chmod 600 $ssh_dir/id_rsa
-    chown $user: $ssh_dir/id_rsa
-    ssh-keygen -y -f $ssh_dir/id_rsa > $ssh_dir/id_rsa.pub
-    cat $ssh_dir/id_rsa.pub >> $ssh_dir/authorized_keys2
-    chmod 644 $ssh_dir/authorized_keys2
-    cat >>$ssh_dir/config <<EOL
-    Host *
-         StrictHostKeyChecking no
-EOL
-    chmod 644 $ssh_dir/config
+    sudo su - $user -c "mkdir ${ssh_dir} && chmod 700 ${ssh_dir}"
+    sudo su - $user -c "/usr/bin/geni-get key > ${ssh_dir}/id_rsa"
+    sudo su - $user -c "chmod 600 ${ssh_dir}/id_rsa"
+    sudo su - $user -c "chown $user: ${ssh_dir}/id_rsa"
+    sudo su - $user -c "ssh-keygen -y -f ${ssh_dir}/id_rsa > ${ssh_dir}/id_rsa.pub"
+    sudo su - $user -c "cat ${ssh_dir}/id_rsa.pub >> $ssh_dir/authorized_keys2"
+    sudo su - $user -c "echo -e '\\nHost * \\n\\tStrictHostKeyChecking no\\n' >> ${ssh_dir}/config"
+    sudo su - $user -c "chmod 644 ${ssh_dir}/config"
 done
 
 
