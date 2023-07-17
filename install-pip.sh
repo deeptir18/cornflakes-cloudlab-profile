2b6ee5a45f7ae2676185da2b006b7f36049ad050
deeptir <deeptir@cs.stanford.edu>
2023-07-17 13:25:47 -0400

    Modified pip and rust to install locally

--- install-pip.sh
+++ install-pip.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+set -x
+SCRIPTNAME=$0
+#
+# Might not be on the local cluster, so need to use the urn to
+# see who the actual creator is.
+#
+GENIUSER=`geni-get user_urn | awk -F+ '{print $4}'`
+if [ $? -ne 0 ]; then
+echo "ERROR: could not run geni-get user_urn!"
+exit 1
+fi
+if [ $USER != $GENIUSER ]; then
+sudo -u $GENIUSER $SCRIPTNAME
+exit $?
+fi
 pip3 install --user colorama gitpython tqdm parse
 pip3 install --user setuptools_rust
 pip3 install --user fabric==2.6.0 
--- install-rust.sh
+++ install-rust.sh
@@ -1,6 +1,23 @@
-USER=$1
-su - $USER -c  "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
-su - $USER -c "rustup install 1.70.0"
-su - $USER -c "rustup default 1.70.0"
-su - $USER -c "rustup component add rustfmt"
-su - $USER -c "rustup default stable"
+#!/bin/bash
+set -x
+SCRIPTNAME=$0
+#
+# Might not be on the local cluster, so need to use the urn to
+# see who the actual creator is.
+#
+GENIUSER=`geni-get user_urn | awk -F+ '{print $4}'`
+if [ $? -ne 0 ]; then
+echo "ERROR: could not run geni-get user_urn!"
+exit 1
+fi
+if [ $USER != $GENIUSER ]; then
+sudo -u $GENIUSER $SCRIPTNAME
+exit $?
+fi
+
+HOMEDIR="/users/$USER" 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+rustup install 1.70.0
+rustup default 1.70.0
+rustup component add rustfmt
+rustup default stable
