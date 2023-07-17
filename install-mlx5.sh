31d7ab685a2374f10097be0397b752b798138d8c
deeptir <deeptir@cs.stanford.edu>
2023-07-17 13:28:11 -0400

    remove ibd restart

--- install-mlx5.sh
+++ install-mlx5.sh
@@ -4,6 +4,6 @@ PACKAGES=$1
 pushd $PACKAGES
 pushd MLNX_OFED_LINUX-5.6-2.0.9.0-ubuntu20.04-x86_64
 sudo ./mlnxofedinstall --upstream-libs --dpdk --force
-sudo /etc/init.d/openibd restart
+#sudo /etc/init.d/openibd restart
 popd
 popd
