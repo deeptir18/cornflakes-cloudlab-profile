7d95b58d5e22e465ab9c28872f58e6c13436ac83
deeptir <deeptir@cs.stanford.edu>
2023-07-17 16:21:44 -0400

    bug in clone script

--- clone_cornflakes.sh
+++ clone_cornflakes.sh
@@ -13,7 +13,7 @@ fi
 
 cd /mydata/$GENIUSER/
 sudo su - $GENIUSER -c 'git clone https://github.com/deeptir18/cornflakes-scripts.git --recursive'
-sudo su - $GENIUSER -c 'cd cornflakes-scripts/cornflakes'
+cd /mydata/$GENIUSER/cornflakes-scripts/cornflakes
 sudo su - $GENIUSER -c 'make submodules CONFIG_MLX5=y'
 sudo su - $GENIUSER -c 'make kv CONFIG_MLX5=y CONFIG_DPDK=y'
 sudo su - $GENIUSER -c 'make redis CONFIG_MLX5=y CONFIG_DPDK=y'
